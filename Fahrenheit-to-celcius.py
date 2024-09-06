76377ff1f6eaad6a889db5a540b53602c1c36871
TanTan1941 <105098069+TanTan1941@users.noreply.github.com>
2024-09-06 01:31:10 -0700

    Create Fahrenheit-to-celcius.py

---
+++ Fahrenheit-to-celcius.py
@@ -0,0 +1,20 @@
+# assign variables
+print("Fahrenheit to celsius calculator! \n")
+intFahrenheit = input("Enter your fahrenheit: ")
+
+# Converting intFahrenheit to float format
+
+intFahrenheit = float(intFahrenheit)
+
+# strCelsius being our formula
+
+strCelsius = (intFahrenheit - 32) * 5/9
+
+#convert strcelsius to string format
+
+strCelsius = str(strCelsius)
+
+#give output
+
+print(strCelsius)
+
